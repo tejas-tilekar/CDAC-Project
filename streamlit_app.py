521987ef5984854ef4b490b86fd1502edc994f16
tejas-tilekar <144610674+tejas-tilekar@users.noreply.github.com>
2024-08-05 15:42:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ Upload the RFM data and get your customer lifetime prediction on the fly !!! :sm
 	""")
 
 
-st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuBMnYjfRUU5fcQQLNqxEBfwPjsoWu2xHLcw&s", use_column_width = True)
+st.image("https://www.retently.com/wp-content/uploads/2017/02/customer-lifetime-value.png", use_column_width = True)
 
 
 data = st.file_uploader("File Uploader")
