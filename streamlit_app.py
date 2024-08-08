61576beb7ac2252e068336b1871cb5a72129924e
tejas-tilekar <144610674+tejas-tilekar@users.noreply.github.com>
2024-08-08 13:56:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ st.image("https://www.retently.com/wp-content/uploads/2017/02/customer-lifetime-
 
 data = st.file_uploader("File Uploader")
 
-st.sidebar.image("https://www.adlibweb.com/wp-content/uploads/2020/06/customer-lifetime-value.jpg", width = 250)
+st.sidebar.image("https://www.adlibweb.com/wp-content/uploads/2020/06/customer-lifetime-value.jpg", width = 150)
 st.sidebar.markdown(""" **Project by Anshu Lalwani & Tejas Tilekar** """)
 
 
@@ -67,7 +67,7 @@ st.sidebar.markdown("""
 
 features = pd.DataFrame(slider_data, index = [0])
 
-st.sidebar.write(features)
+#st.sidebar.write(features)
 
 st.sidebar.markdown("""
 
